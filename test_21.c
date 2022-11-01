d0b61e6d20ebba2f578c7dd1262d6fa6279f1b58
mio-lsj <117135706+mio-lsj@users.noreply.github.com>
2022-11-01 15:44:35 +0800

    for

---
+++ test_21.c
@@ -0,0 +1,13 @@
+# define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 0 };
+	int a = 0;
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d\n", arr[a]);
+
+	}
+	return 0;
+}

05e9a402b8f56a4517a774704ca5567572533c77
mio-lsj <117135706+mio-lsj@users.noreply.github.com>
2022-11-01 19:41:37 +0800

    1—100有多少个数字9

---
+++ test_61.c
@@ -0,0 +1,16 @@
+# define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+    int a = 0;
+    int count = 0;
+    for (a = 1; a <= 100; a++)
+    {
+        if (a % 10 == 9)
+            count++;
+        if (a / 10 == 9)
+            count++;
+    }
+    printf("%d\n", count);
+    return 0;
+}

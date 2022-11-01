b4ec14b3f26a3fd3629d1f6d5a69e0bdc19ea9f2
mio-lsj <117135706+mio-lsj@users.noreply.github.com>
2022-11-01 19:21:44 +0800

    strcpy拷贝字符

---
+++ test_39.c
@@ -0,0 +1,11 @@
+# define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char arr1[20] = { 0 };
+    char arr2[] = "hello";
+    strcpy(arr1, arr2);
+    printf("%s", arr1);//´òÓ¡×Ö·û´®ÓÃ%s
+    return 0;
+}

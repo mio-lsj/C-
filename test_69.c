768b9919191115832be2b4b3718e81f0b6fee46e
mio-lsj <117135706+mio-lsj@users.noreply.github.com>
2022-11-01 19:52:23 +0800

    逆序 循环

---
+++ test_69.c
@@ -0,0 +1,35 @@
+# define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int my_strlen(char* str)
+{
+    int count = 0;
+    while (*str != '\0')
+    {
+        count++;
+        str++;
+    }
+
+    return count;
+}
+
+void reverse_string(char* str)
+{
+    int left = 0;
+    int right = my_strlen(str) - 1;
+    while (left < right)
+    {
+        char tmp = str[left];
+        str[left] = str[right];
+        str[right] = tmp;
+        left++;
+        right--;
+    }
+}
+
+int main()
+{
+    char arr[] = "abcdef";
+    reverse_string(arr);
+    printf("%s\n", arr);
+    return 0;
+}

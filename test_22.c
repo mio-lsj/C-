569b8f86cf43271621b3502630c7d997a91d320d
mio-lsj <117135706+mio-lsj@users.noreply.github.com>
2022-11-01 15:45:29 +0800

    for

---
+++ test_22.c
@@ -0,0 +1,12 @@
+# define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int x = 0;
+	int y = 0;
+	for (x = 0, y = 0; x < 2 && y < 5; x++, y++)
+	{
+		printf("hehe\n");
+	}
+	return 0;
+}

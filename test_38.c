6b6b05c23d49525bbed3cc4f29b2a949f136bbbb
mio-lsj <117135706+mio-lsj@users.noreply.github.com>
2022-11-01 19:20:41 +0800

    practice

---
+++ test_38.c
@@ -0,0 +1,13 @@
+# define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+void main()
+{
+    int b, c = 5;
+    int a;
+    a = 1;
+    b = c + 1;
+    printf("%d\n", b);
+    double d = c / 2.0;
+    printf("%f\n", a + b + d);
+    return 0;
+}

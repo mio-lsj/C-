54ebec52ff694c3fbb5ef2de415bc63ec6f1871a
mio-lsj <117135706+mio-lsj@users.noreply.github.com>
2022-11-01 15:14:08 +0800

    if else
    if else的应用

---
+++ test_2.c
@@ -0,0 +1,13 @@
+# define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	scanf("%d %d", &a, &b);
+	if (a > b)
+		printf("%d\n", a);
+	else
+		printf("%d\n", b);
+	return 0;
+}

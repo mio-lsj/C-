39894771e6330da308fe50936741f78180828533
mio-lsj <117135706+mio-lsj@users.noreply.github.com>
2022-11-04 21:57:51 +0800

    三子棋

---
+++ test_84.c
@@ -0,0 +1,83 @@
+# define _CRT_SECURE_NO_WARNINGS 1
+#include"game.h"
+void menu()
+{
+	printf("**********************************\n");
+	printf("********    1. play    ***********\n");
+	printf("********    0. exit    ***********\n");
+	printf("**********************************\n");
+}
+
+void game()
+{
+	char ret = 0;
+
+	//存储数据 - 二维数组
+	char board[ROW][COL];
+
+	//初始化棋盘2 - 初始化空格
+	InitBoard(board, ROW, COL);
+
+	//打印棋盘 - 本质是打印数组内容
+	DisplayBoard(board,ROW,COL);
+
+	while (1)
+	{
+		//玩家下棋
+		PlayerMove(board,ROW,COL);
+		DisplayBoard(board, ROW, COL);
+
+		//判断输赢
+		ret = IsWin(board,ROW,COL);
+		if (ret != 'c')
+			break;
+
+		//电脑下棋
+		ComputerMove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+
+		//判断输赢
+		ret = IsWin(board, ROW, COL);
+		if (ret != 'c')
+			break;
+	}
+	if (ret == '*')
+	{
+		printf("玩家赢了\n");
+	}
+	else if (ret == '#')
+	{
+		printf("电脑赢了\n");
+	}
+	else if (ret == 'Q')
+	{
+		printf("平局\n");
+	}
+	DisplayBoard(board, ROW, COL);
+}
+
+int main()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+
+	do
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("输入错误，请重新选择\n");
+			break;
+		}
+	} while (input);
+	return 0;
+}

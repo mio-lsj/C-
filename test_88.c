7dce3c70855d9d48bc9a93122922d5b2a6229810
mio-lsj <117135706+mio-lsj@users.noreply.github.com>
2022-11-08 09:12:29 +0800

    操作符

---
+++ test_88.c
@@ -0,0 +1,69 @@
+# define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	//float a = 6 / 5.0;
+//	//printf("&f\n", a);
+//	
+//	//int a = 7 % 3;
+//	//printf("%d\n", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	//int a = 1;
+//	////把a的二进制位向左移动一位
+//	////相当于 b = 2*a
+//	//int b = a << 1;
+//	//printf("%d\n", b);
+//	//return 0;
+//
+//	int a = 56;
+//	//相当于a/2
+//	//算数右移
+//	int b = a >> 1;
+//	printf("b=%d\n", b);
+//}
+
+//int main()
+//{
+//	//int a = 3;
+//	//int b = 5;
+//	////& - 按（2进制位）位与
+//	//int c = a & b;
+//
+//	// | - 按（2进制）位或
+//	//int a = 3;
+//	//int b = 5;
+//	//int c = a | b;
+//	//printf("c=%d\n", c);
+//
+//	int a = 3;
+//	int b = 5;
+//	//按（2进制）位异或
+//	//对应的2进制位异或
+//	//相同为0，相异为1
+//	int c = a ^ b;
+//	printf("c=%d\n", c);
+//	return 0;
+//}
+
+//int main()
+//{
+//	//交换22222222222
+//	int a = 3;
+//	int b = 5;
+//
+//	//数值太大，无法计算
+//	//a = a + b;
+//	//b = a - b;
+//	//a = a - b;
+//
+//	a = a ^ b;
+//	b = a ^ b;
+//	a = a ^ b;
+//	printf("a=%d ,b=%d", a, b);
+//	return 0;
+//}
+

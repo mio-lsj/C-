d2c1c009e2814dfaedb553696c11b3b3b34a6408
mio-lsj <117135706+mio-lsj@users.noreply.github.com>
2022-11-10 15:48:30 +0800

    二级指针，指针数组

---
+++ test_102.c
@@ -0,0 +1,27 @@
+# define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//二级指针
+//int main()
+//{
+//	int a = 10;
+//	int* pa = &a;//pa是指针变量，一级指针
+//	//ppa就是二级指针变量
+//	int** ppa = &pa;//pa也是变量,&pa去除pa在内存中的起始地址
+//	int*** pppa = &ppa;//三级指针
+//
+//	return 0;
+//}
+
+
+
+//指针数组 - 数组
+int main()
+{
+	int arr[10];//整型数组 - 存放整型
+	char ch[5];//字符数组 - 存放字符
+	//指针数组 - 存放指针的数组
+	int* parr[5];//整型指针的数组
+	char* pch[5];
+	return 0;
+}

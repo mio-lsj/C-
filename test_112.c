b9017bbc606da8a08bf88d299158b1c6695bbb4f
mio-lsj <117135706+mio-lsj@users.noreply.github.com>
2022-11-17 21:46:01 +0800

    较优秀的代码

---
+++ test_112.c
@@ -0,0 +1,15 @@
+﻿# define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+void my_strlen(char* arr1, char* arr2)
+{
+	while (*arr1++ = *arr2++);
+}
+
+int main()
+{
+	char arr1[20] = { "xxxxxxxxx"};
+	char arr2[] = { "hello" };
+	my_strlen(arr1, arr2);
+	printf("%s\n", arr1);
+	return 0;
+}
